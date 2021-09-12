409de0d918fc8ecde5f4afe11dc1318e62331867
zio-dev <is.in.776dev@gmail.com>
2021-09-12 14:01:28 +0900

    Vue tutorial4 done.

---
+++ tutorial4/index.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue.js!'
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+      // this._data.message = this.message.split('').reverse().join('')
+    }
+  }
+})
